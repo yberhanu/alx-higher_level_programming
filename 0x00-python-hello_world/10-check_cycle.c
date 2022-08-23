8903e056f211af94511179eeaee1cf9ea634339d
yberhanu <yohannes.birh@gmail.com>
2022-08-22 18:21:49 -0700

    Fix indentation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,38 @@
 #include "lists.h"
 /**
-*check_cycle - checks for cycle in linked list
-*@list: list of  or head of the list
-*Return: 0 if no cycle otherwise 1
+* check_cycle - checks if a singly linked list has
+* a cycle in it
+* @list: pointer to the list
+* Return: 0 if there is no cycle,
+* 1 if there is a cycle
 */
 int check_cycle(listint_t *list)
 {
-listint_t *p1, *p2;
-if (list == NULL || list->next == NULL)
-return (0);
-p1 = list->next;
-p2 = list->next->next;
-while (p1 && p2 && p2->next)
+listint_t *p2;
+listint_t *prev;
+p2 = list;
+prev = list;
+while (list && p2 && p2->next)
 {
-if (p1 == p2)
-return (1);
-p1 = p1->next;
+list = list->next;
 p2 = p2->next->next;
+if (list == p2)
+{
+list = prev;
+prev =  p2;
+while (1)
+{
+p2 = prev;
+while (p2->next != list && p2->next != prev)
+{
+p2 = p2->next;
+}
+if (p2->next == list)
+break;
+list = list->next;
+}
+return (1);
+}
 }
 return (0);
 }
