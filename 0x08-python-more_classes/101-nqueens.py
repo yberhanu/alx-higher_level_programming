c624356063a57aa2a3d62c97c6fb4daf2d9ae5ce
yberhanu <104769142+yberhanu@users.noreply.github.com>
2022-09-20 23:44:24 +0300

    Rewrite the code

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,111 +1,56 @@
 #!/usr/bin/python3
-
 """ Queen """
-
-
-
 
 
 import sys
 
 
-
-
-
 def nqueens(size):
-    
     """Check and initialzation"""
-    
     if type(size) is not int:
-        
         print("N must be a number")
-        
         return
-    
     if size < 4:
-        
         print("N must be at least 4")
-        
         return
-    
     queens = [0] * size
-    
 
-    
     def printsolution(queens):
-        
         """ prints all posssible """
-        
         print("[[0, ", queens[0], "]", sep="", end="")
-        
         for y, x in enumerate(queens[1:], 1):
-            
             print(", [", y, ", ", x, "]", sep="", end="")
-            
         print("]")
-        
 
-        
     def queencalc(queen):
-        
         """Backtracking checker"""
-        
         for x in range(size):
-            
             """horizontal board positions per queen"""
-            
             nextx = 0
-            
             for y in range(queen):
-                
                 qx = queens[y]
-                
                 if x == qx or x + queen == qx + y or x - queen == qx - y:
-                    
                     nextx = 1
-                    
                     break
-                
             if nextx == 1:
-                
                 nextx == 0
-                
                 continue
-            
             if queen != size - 1:
-                
                 queens[queen + 1] = 0
-                
                 queens[queen] = x
-                
                 queencalc(queen + 1)
-                
             else:
-                
                 queens[queen] = x
-                
                 printsolution(queens)
-                
     queencalc(0)
-    
 
-    
 
-    
 if len(sys.argv) != 2:
-    
     print("Usage: nqueens N")
-    
     exit()
-    
 try:
-    
     size = int(sys.argv[1])
-    
 except (ValueError, TypeError):
-    
     print("N must be a number")
-    
     exit()
-    
 nqueens(size)
